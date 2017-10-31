17fc9b8c1fb47efe56f961739ea86afec8d21d8f
joao <joaoantonioneto@outlook.com>
2017-10-31 19:42:18 -0300

    finalizando frequencia

--- faltas/urls.py
+++ faltas/urls.py
@@ -6,4 +6,6 @@ app_name = 'faltas'
 urlpatterns = [
     url(r'^new/(?P<turma_id>[0-9]+)/$', views.new, name='new'),
     url(r'^new/(?P<turma_id>[0-9]+)/cadastrarFrequencia', views.cadastrarFrequencia, name='cadastrarFrequencia'),
+    url(r'^dadosFrequencia/(?P<turma_id>[0-9]+)/$', views.dadosFrequencia, name='dadosFrequencia'),
+    url(r'^faltaAluno/(?P<turma_id>[0-9]+)/$', views.faltaAluno, name='faltaAluno'),
     ]
--- faltas/views.py
+++ faltas/views.py
@@ -20,7 +20,7 @@ def new(request,turma_id):
 	user = request.user
 	usuario = Usuario.objects.get(user=user)
 	turma = Turma.objects.get(pk=turma_id)
-	alunos = turma.alunos.all()
+	alunos = turma.alunos.all().order_by('nome_completo')
 	context= {'turma': turma, 
 	'alunos': alunos, 
 	'usuario':usuario}
@@ -31,17 +31,53 @@ def cadastrarFrequencia(request,turma_id):
 	user = request.user
 	usuario = Usuario.objects.get(user=user)
 	turma = Turma.objects.get(pk=turma_id)
-	alunos = turma.alunos.all()
+	alunos = turma.alunos.all().order_by('nome_completo')
 	frequencia = request.POST.getlist('frequencia[]')
 	alunos_id = request.POST.getlist('alunos[]')
 	for i in range(len(alunos_id)):
 		aluno = Aluno.objects.get(pk=alunos[i])
-		if (frequencia[i] == "Presente") :
+		if (frequencia[i].upper() == "PRESENTE") :
 			presenca = Falta(alunos=aluno, turmas=turma, presenca=True)
 		else:
-			presenca = Falta(alunos=aluno,turmas=turma,presenca=False)
+			presenca = Falta(alunos=aluno,turmas=turma)
 		presenca.save()
+	messages.add_message(request, messages.INFO, 'Frequência cadastrada com sucesso.')
 	context= {'turma': turma, 
 	'alunos': alunos, 
 	'usuario':usuario}
-	return render (request,'faltas/cadastrarFrequencia.html', context=context)
+	html = render_to_string('aluno_area/verficarRespostas.html', context=context)
+	return HttpResponse(html)
+
+def dadosFrequencia(request,turma_id):
+	user = request.user
+	usuario = Usuario.objects.get(user=user)
+	turma = Turma.objects.get(pk=turma_id)
+	alunos = turma.alunos.all().order_by('nome_completo')
+	faltas =[]
+	nome_alunos=[]
+	for i in range(len(alunos)):
+		nome_alunos.append(alunos[i].nome_completo)
+		falta = Falta.objects.filter(turmas=turma,alunos=alunos[i],presenca=False)
+
+		count_falta = len(falta)
+		faltas.append(count_falta)
+
+	context ={'usuario': usuario,
+	'turma':turma,
+	'faltas': json.dumps(faltas),
+	'nome_alunos': json.dumps(nome_alunos),
+	}
+	return render(request,'faltas/dadosFrequencia.html',context=context)
+
+def faltaAluno(request,turma_id):
+	user = request.user
+	usuario = Usuario.objects.get(user=user)
+	turma = Turma.objects.get(pk=turma_id)
+	faltas = Falta.objects.filter(turmas=turma,alunos=usuario).order_by('date')
+	count_falta = len(Falta.objects.filter(turmas=turma,alunos=usuario,presenca=False))
+	context ={'usuario': usuario,
+	'turma':turma,
+	'faltas': faltas,
+	'count_falta':count_falta
+	}
+	return render(request,'faltas/faltaAluno.html',context=context)
