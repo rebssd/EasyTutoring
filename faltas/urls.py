d932deda02079879f1b9a4614b8946114c566f8f
joao <joaoantonioneto@outlook.com>
2017-10-31 11:06:55 -0300

    cadastrando falta

--- faltas/urls.py
+++ faltas/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'faltas'
 urlpatterns = [
     url(r'^new/(?P<turma_id>[0-9]+)/$', views.new, name='new'),
+    url(r'^new/(?P<turma_id>[0-9]+)/cadastrarFrequencia', views.cadastrarFrequencia, name='cadastrarFrequencia'),
     ]
--- faltas/views.py
+++ faltas/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from usuarios.models import Usuario, Professor
+from usuarios.models import Usuario, Professor,Aluno
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Permission
 from disciplinas.models import Disciplina
+from faltas.models import Falta
 from turmas.models import Turma
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -24,3 +25,23 @@ def new(request,turma_id):
 	'alunos': alunos, 
 	'usuario':usuario}
 	return render(request,'faltas/new.html',context=context)
+
+
+def cadastrarFrequencia(request,turma_id):
+	user = request.user
+	usuario = Usuario.objects.get(user=user)
+	turma = Turma.objects.get(pk=turma_id)
+	alunos = turma.alunos.all()
+	frequencia = request.POST.getlist('frequencia[]')
+	alunos_id = request.POST.getlist('alunos[]')
+	for i in range(len(alunos_id)):
+		aluno = Aluno.objects.get(pk=alunos[i])
+		if (frequencia[i] == "Presente") :
+			presenca = Falta(alunos=aluno, turmas=turma, presenca=True)
+		else:
+			presenca = Falta(alunos=aluno,turmas=turma,presenca=False)
+		presenca.save()
+	context= {'turma': turma, 
+	'alunos': alunos, 
+	'usuario':usuario}
+	return render (request,'faltas/cadastrarFrequencia.html', context=context)
